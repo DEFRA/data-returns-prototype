1c316e442996ff85f196dd49cb4020dfef09db22
Jeremy Gillies <jeremygillies@gmail.com>
2017-07-28 15:05:23 +0100

    PI - change details html

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Data returns sandbox',
+  serviceName: 'Report environmental data',
 
   // Default port that prototype runs on
   port: '3000',
