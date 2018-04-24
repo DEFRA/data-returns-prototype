f95de00d08b67c88352e9861a60ce6f153b15b99
Liam McMurray <liammcmurray@gmail.com>
2018-04-24 15:51:46 +0100

    update to releases journey - now with branching

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,19 @@ router.get('/', function (req, res) {
 })
 
 // add your routes here
+// Branching
+
+router.get('/releases/air/choose', function (req, res) {
+  // get the answer from the query string (eg. ?release=false)
+  var releaseair = req.query.releaseair
+
+  if (releaseair === 'no') {
+    // redirect to the relevant page
+    res.redirect('/task-list')
+  } else {
+    // if releaseair is any other value (or is missing) render the page requested
+    res.render('releases/air/choose-substance')
+  }
+})
 
 module.exports = router
